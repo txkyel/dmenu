3aaab7a2be340cfabab994fb44e4d0165c9282ed
Kyle <kylexiao20@gmail.com>
2021-12-13 00:54:42 +0000

    Rework dmenu Xresources colours

--- config.def.h
+++ config.def.h
@@ -16,11 +16,14 @@ static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
 static char selfgcolor[]  = "#eeeeee";
 static char selbgcolor[]  = "#005577";
+static char outfgcolor[]  = "#000000";
+static char outbgcolor[]  = "#00ffff";
+
 static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { normfgcolor, normbgcolor },
 	[SchemeSel]  = { selfgcolor,  selbgcolor  },
-	[SchemeOut]  = { "#000000",   "#00ffff" },
+	[SchemeOut]  = { outfgcolor,  outbgcolor  },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
@@ -37,9 +40,11 @@ static const char worddelimiters[] = " ";
 ResourcePref resources[] = {
 	{ "font",        STRING, &font },
 	{ "font2",       STRING, &font2 },
-	{ "color4",      STRING, &normfgcolor },
+	{ "color7",      STRING, &normfgcolor },
 	{ "color0",      STRING, &normbgcolor },
-	{ "color0",      STRING, &selfgcolor },
-	{ "color4",      STRING, &selbgcolor },
+	{ "color7",      STRING, &selfgcolor },
+	{ "color1",      STRING, &selbgcolor },
+	{ "color7",      STRING, &outfgcolor },
+	{ "color6",      STRING, &outbgcolor },
 	{ "prompt",      STRING, &prompt },
 };
